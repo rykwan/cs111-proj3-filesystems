14273bf246d50bfa7c9e1f8c509ca1aa52034c7b
rykwan <raykwan@ucla.edu>
2018-05-31 16:05:49 -0700

    hardlink in makefile, open file function in python

--- proj3B/lab3b.py
+++ proj3B/lab3b.py
@@ -2,20 +2,26 @@
 
 import sys
 
-def main():
+def getFileArg(args): # checks number of args and returns opened file
     usage_msg = "./lab3b CSV_FILE"
-    if len(sys.argv) != 2:
+    if len(args) != 2:
         print("Wrong number of arguments.")
         print(usage_msg)
         exit(1)
 
-    csvfile = sys.argv[1]
+    filename = args[1]
     try:
-        open(csvfile, "r")
+        return open(filename, "r")
     except IOError:
-        print("Error: could not open file '%s'" %csvfile)
+        print("Error: could not open file '%s'" %filename)
         exit(1)
+
+def main():
+    csvfile = getFileArg(sys.argv)
+
     
+    csvfile.close()
+    exit(0)
 
 if __name__ == "__main__":
     main()
