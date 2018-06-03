15eed9dbcf6ed0e1645ed81f7a2acdd125f41592
rykwan <raykwan@ucla.edu>
2018-06-02 18:05:33 -0700

    print to stderr for error message

--- proj3B/lab3b.py
+++ proj3B/lab3b.py
@@ -3,17 +3,17 @@
 import sys
 
 def getFileArg(args): # checks number of args and returns opened file
-    usage_msg = "./lab3b CSV_FILE"
+    usage_msg = "./lab3b CSV_FILE\n"
     if len(args) != 2:
-        print("Wrong number of arguments.")
-        print(usage_msg)
+        sys.stderr.write("Wrong number of arguments.\n")
+        sys.stderr.write(usage_msg)
         exit(1)
 
     filename = args[1]
     try:
         return open(filename, "r")
     except IOError:
-        print("Error: could not open file '%s'" %filename)
+        sys.stderr.write("Error: could not open file '%s'\n" %filename)
         exit(1)
 
 def main():
