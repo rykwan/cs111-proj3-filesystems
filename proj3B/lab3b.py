b11e0dff65e403c3c1c255b892c71efb06c769e5
rykwan <raykwan@ucla.edu>
2018-06-04 22:53:10 -0700

    started block audit

--- proj3B/lab3b.py
+++ proj3B/lab3b.py
@@ -33,98 +33,98 @@ def getFileArg(args): # checks number of args and returns opened file
 class Superblock:
     def __init__(self, line):
         self.name = "SUPERBLOCK"
-        list = listConvert(line)
-        if list[0] != self.name:
+        linelist = listConvert(line)
+        if linelist[0] != self.name:
             initError(self.name, line)
             return
-        self.numBlocks = list[1]
-        self.numInodes = list[2]
-        self.blockSize = list[3]
-        self.nodeSize = list[4]
-        self.blocksPerGroup = list[5]
-        self.nodesPerGroup = list[6]
-        self.firstInode = list[7]
+        self.numBlocks = linelist[1]
+        self.numInodes = linelist[2]
+        self.blockSize = linelist[3]
+        self.nodeSize = linelist[4]
+        self.blocksPerGroup = linelist[5]
+        self.nodesPerGroup = linelist[6]
+        self.firstInode = linelist[7]
 
 class Group:
     def __init__(self, line):
         self.name = "GROUP"
-        list = listConvert(line)
-        if list[0] != self.name:
+        linelist = listConvert(line)
+        if linelist[0] != self.name:
             initError(self.name, line)
             return
-        self.groupNum = line[1]
-        self.numBlocks = line[2]
-        self.numInodes = line[3]
-        self.numFreeBlocks = line[4]
-        self.numFreeInodes = line[5]
-        self.blockBitmapBlockNum = line[6]
-        self.nodeBitmapBlockNum = line[7]
-        self.firstInodeBlockNum = line[8]
+        self.groupNum = linelist[1]
+        self.numBlocks = linelist[2]
+        self.numInodes = linelist[3]
+        self.numFreeBlocks = linelist[4]
+        self.numFreeInodes = linelist[5]
+        self.blockBitmapBlockNum = linelist[6]
+        self.nodeBitmapBlockNum = linelist[7]
+        self.firstInodeBlockNum = linelist[8]
 
 class FreeBlock:
     def __init__(self, line):
         self.name = "BFREE"
-        list = listConvert(line)
-        if list[0] != self.name:
+        linelist = listConvert(line)
+        if linelist[0] != self.name:
             initError(self.name, line)
             return
-        self.numFreeBlock = list[1]
+        self.numFreeBlock = linelist[1]
 
 class FreeInode:
     def __init__(self, line):
         self.name = "IFREE"
-        list = listConvert(line)
-        if list[0] != self.name:
+        linelist = listConvert(line)
+        if linelist[0] != self.name:
             initError(self.name, line)
             return
-        self.numFreeInode = list[1]
+        self.numFreeInode = linelist[1]
 
 class Inode:
     def __init__(self, line):
         self.name = "INODE"
-        list = listConvert(line)
-        if list[0] != self.name:
+        linelist = listConvert(line)
+        if linelist[0] != self.name:
             initError(self.name, line)
             return
-        self.inodeNum = list[1]
-        self.fileType = list[2]
-        self.mode = list[3]
-        self.owner = list[4]
-        self.group = list[5]
-        self.linkCount = list[6]
-        self.ctime = list[7]
-        self.mtime = list[8]
-        self.atime = list[9]
-        self.fileSize = list[10]
-        self.numBlocks = list[11]
-        self.blockPointers = list[12:]
+        self.inodeNum = linelist[1]
+        self.fileType = linelist[2]
+        self.mode = linelist[3]
+        self.owner = linelist[4]
+        self.group = linelist[5]
+        self.linkCount = linelist[6]
+        self.ctime = linelist[7]
+        self.mtime = linelist[8]
+        self.atime = linelist[9]
+        self.fileSize = linelist[10]
+        self.numBlocks = linelist[11]
+        self.blockPointers = linelist[12:]
 
 class DirectoryEntry:
     def __init__(self, line):
         self.name = "DIRENT"
-        list = listConvert(line)
-        if list[0] != self.name:
+        linelist = listConvert(line)
+        if linelist[0] != self.name:
             initError(self.name, line)
             return
-        self.parentInodeNum = list[1]
-        self.logicalByteOffset = list[2]
-        self.inodeNum = list[3]
-        self.entryLen = list[4]
-        self.nameLen = list[5]
-        self.name = list[6]
+        self.parentInodeNum = linelist[1]
+        self.logicalByteOffset = linelist[2]
+        self.inodeNum = linelist[3]
+        self.entryLen = linelist[4]
+        self.nameLen = linelist[5]
+        self.name = linelist[6]
 
 class IndirectBlock:
     def __init__(self, line):
         self.name = "INDIRECT"
-        list = listConvert(line)
-        if list[0] != self.name:
+        linelist = listConvert(line)
+        if linelist[0] != self.name:
             initError(self.name, line)
             return
-        self.inodeNum = list[1]
-        self.indirectionLevel = list[2]
-        self.logicalBlockOffset = list[3]
-        self.indirBlockNum = list[4]
-        self.refBlockNum = list[5]
+        self.inodeNum = linelist[1]
+        self.indirectionLevel = linelist[2]
+        self.logicalBlockOffset = linelist[3]
+        self.indirBlockNum = linelist[4]
+        self.refBlockNum = linelist[5]
 
 class FileSystem:
     def __init__(self, csvFile):
@@ -179,11 +179,61 @@ def inodeAudit(fs):
         if (inodes[inodeNum] == False and inodeNum not in freeList):
             sys.stdout.write("UNALLOCATED INODE " + str(inodeNum) + " NOT ON FREELIST\n")
 
+class BlockObject:
+    def __init__(self, blockNum, inoNum, indirection):
+        self.blockNum = blockNum
+        self.inodeNum = inoNum
+        self.indirection = indirection
+
+def printInvalidOrReservedBlock(INorRE, blockno, idx, inonum): # 0 for invalid, 1 for reserved
+    offset = 0
+    if INorRE == 0:
+        sys.stdout.write("INVALID ")
+    else:
+        sys.stdout.write("RESERVED ")
+    if idx == 12:  #TODO: how to get logical offset
+        sys.stdout.write("INDIRECT ")
+    elif idx == 13:
+        sys.stdout.write("DOUBLE ")
+    elif idx == 14:
+        sys.stdout.write("TRIPLE ")
+    sys.stdout.write("BLOCK %d IN INODE %d AT OFFSET %d\n" % (blockno, inonum, offset) )
+
+def blockAudit(fs):
+    allBlocks = dict((b, []) for b in range(1,fs.superblock.numBlocks+1) )
+
+    for frb in fs.freeBlocks:
+        allBlocks[frb.numFreeBlock] = None # set free blocks to None
+
+    for ino in fs.inodes:
+        for (idx, bp) in enumerate(ino.blockPointers):
+            indirection = idx - 11 # indirection: 1 for indirect, 2 for double, 3 for triple
+            if bp in allBlocks:
+                if allBlocks[bp] == None:
+                    sys.stdout.write("ALLOCATED %d ON FREELIST\n" % bp)
+                    allBlocks[bp] = []
+
+            if bp < 0 or bp > fs.superblock.numBlocks:
+                printInvalidOrReservedBlock(0, bp, idx, ino.inodeNum)
+            elif bp == fs.groups[0].blockBitmapBlockNum or bp == fs.groups[0].nodeBitmapBlockNum or (bp != 0 and bp < int(fs.groups[0].firstInodeBlockNum)):
+                printInvalidOrReservedBlock(1, bp, idx, ino.inodeNum)
+            elif bp != 0:
+                allBlocks.setdefault(bp,[]).append(BlockObject(bp, ino.inodeNum, indirection))
+
+   # for b in range(1,fs.superblock.numBlocks+1):
+   #     if allBlocks[b] 
+
+
+
+
+
 def main():
     csvfile = getFileArg(sys.argv)
 
     fs = FileSystem(csvfile)
     inodeAudit(fs)
+
+    blockAudit(fs)
 
     csvfile.close()
     exit(0)
